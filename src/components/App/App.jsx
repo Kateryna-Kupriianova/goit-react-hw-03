88adf4ac5221140bf15bb4b0ffc8fc5c10b70c25
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-08-07 21:20:02 +0300

    add default contacts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,18 +6,19 @@ import { useState, useEffect } from 'react';
 
 
 const App = () => {
-
+    const defaultContacts = [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
     const [contacts, setContacts] = useState(() => {
         try {
             const storedContacts = localStorage.getItem('contacts');
-            return storedContacts ? JSON.parse(storedContacts) : [];
+            return storedContacts ? JSON.parse(storedContacts) : defaultContacts;
         } catch (error) {
             console.error("Error parsing contacts from localStorage:", error);
-            return [
-                { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-                { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-                { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-                { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },];
+            return defaultContacts;
         }
     });
 
